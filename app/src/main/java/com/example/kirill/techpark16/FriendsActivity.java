6575794b2a1dd95785b563f134400a798170981b
Kirill Matveev <kirill@Air-Kirill.Dlink>
2016-02-21 10:42:19 +0300

    getting friend list

--- app/src/main/java/com/example/kirill/techpark16/FriendsActivity.java
+++ app/src/main/java/com/example/kirill/techpark16/FriendsActivity.java
@@ -4,18 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
 
 /**
  * Created by kirill on 18.02.16.
  */
-public class FriendsActivity extends Activity {//FragmentActivity{
+public class FriendsActivity extends FragmentActivity{
+
+    private String [] scope = new String[] {VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_fragments_activity);
+
+        VKSdk.login(this, scope);
 
         final Button button1 = (Button) findViewById(R.id.test_friend_btn);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +45,40 @@ public class FriendsActivity extends Activity {//FragmentActivity{
                 startActivity(activity1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                final ListView listView = (ListView) findViewById(R.id.listViewFriends);
+                VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKList list = (VKList) response.parsedModel;
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(FriendsActivity.this,
+                                android.R.layout.simple_expandable_list_item_1, list);
+
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+                );
+
+                Toast.makeText(getApplicationContext(), "Good", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(), "Not Good", Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
--- app/src/main/java/com/example/kirill/techpark16/MainActivity.java
+++ app/src/main/java/com/example/kirill/techpark16/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.kirill.techpark16;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
 import com.vk.sdk.util.VKUtil;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private String [] scope = new String[] {VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
 
@@ -23,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        VKSdk.login(this,scope);
+        //VKSdk.login(this,scope);
 
 //        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
 //        System.out.println(Arrays.asList(fingerprints));
