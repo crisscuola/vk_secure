5fa7ef1895efb927d824efe5ff2611181b283985
Konstantin Ermakov <bestwick91@gmail.com>
2016-02-22 21:20:26 +0300

    my test coomit

--- app/src/main/java/com/example/kirill/techpark16/DialogsListActivity.java
+++ app/src/main/java/com/example/kirill/techpark16/DialogsListActivity.java
@@ -98,7 +98,7 @@ public class DialogsListActivity extends AppCompatActivity {
             @Override
             public void onResult(VKAccessToken res) {
 // Пользователь успешно авторизовался
-                // test commit
+                // test commit la la
 
 
             }
