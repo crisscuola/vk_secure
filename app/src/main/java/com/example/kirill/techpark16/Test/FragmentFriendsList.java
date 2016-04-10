1f10015112a4a649ad719f25d98dad0b628ee9a9
Konstantin Ermakov <bestwick91@gmail.com>
2016-04-10 13:19:02 +0300

    friends list work

--- app/src/main/java/com/example/kirill/techpark16/Test/FragmentFriendsList.java
+++ app/src/main/java/com/example/kirill/techpark16/Test/FragmentFriendsList.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-import com.example.kirill.techpark16.FriendListAdapter;
 import com.vk.sdk.api.VKApi;
 import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKParameters;
@@ -29,6 +29,13 @@ public class FragmentFriendsList extends ListFragment {
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
         VKRequest request_list_friend = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name", "order", "hints"));
 
         request_list_friend.executeWithListener(new VKRequest.VKRequestListener() {
@@ -38,18 +45,13 @@ public class FragmentFriendsList extends ListFragment {
                 super.onComplete(response);
 
                 list = (VKList) response.parsedModel;
-
-
-//                listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1, list));
-
-                setListAdapter(new FriendListAdapter(inflater.getContext(),list));
-
+                
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
+                setListAdapter(adapter);
 
             }
         });
 
-
-        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
 }
