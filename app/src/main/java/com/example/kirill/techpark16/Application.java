de450c2181cec6b3d167b92f115061e9092897a4
Kirill Matveev <kmatveev15@gmail.com>
2016-04-20 12:50:18 +0300

    str encryprion

--- app/src/main/java/com/example/kirill/techpark16/Application.java
+++ app/src/main/java/com/example/kirill/techpark16/Application.java
@@ -34,24 +34,5 @@ public class Application extends android.app.Application {
         vkAccessTokenTracker.startTracking();
         VKSdk.initialize(this);
 
-        RSAEncryption rsaInstance = new RSAEncryption();
-        String message = "Hello";
-        try {
-            byte[] enc = rsaInstance.encrypt(message);
-            String decr = rsaInstance.decrypt(enc);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
     }
 }
