54484a7a605d5622bf7b914a3074f069738266bc
kirill-m <kmatveev15@gmail.com>
2016-05-07 21:53:03 +0300

    friendlist refactor

--- app/src/main/java/com/example/kirill/techpark16/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/kirill/techpark16/Adapters/RecyclerViewAdapter.java
@@ -1,14 +1,28 @@
 package com.example.kirill.techpark16.Adapters;
 
 import android.content.Context;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kirill.techpark16.ChatMessage;
+import com.example.kirill.techpark16.Fragments.FragmentSingleFriend;
 import com.example.kirill.techpark16.R;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiModel;
+import com.vk.sdk.api.model.VKList;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -16,27 +30,97 @@ import java.util.ArrayList;
  * Created by kirill on 07.05.16.
  */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private Context mContext;
-    private ArrayList<String> friendList = new ArrayList<>();
+    private VKList friendList = new VKList();
+    private FragmentManager fragmentManager;
 
-    public  RecyclerViewAdapter(Context  context, ArrayList<String > friendList){
-        this.mContext = context;
+    public  RecyclerViewAdapter(FragmentManager fragmentManager, VKList friendList){
+        this.fragmentManager = fragmentManager;
         this.friendList = friendList;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView txtView;
+        public MyViewHolderClicks mListener;
+
+
+        public MyViewHolder(View itemLayoutView, MyViewHolderClicks listener) {
+            super(itemLayoutView);
+            mListener = listener;
+            txtView = (TextView) itemLayoutView.findViewById(R.id.friend_name);
+            itemLayoutView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onPotato(v);
+        }
+
+        public static interface MyViewHolderClicks {
+            public void onPotato(View caller);
+        }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemLayoutView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.friendlist_item, null);
-
         ViewHolder viewHolder = new ViewHolder(itemLayoutView);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(friendList.get(position));
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.textView.setText(String .valueOf(friendList.get(position)));
 
+        holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("click", String.valueOf(holder.textView.getText()));
+
+                VKRequest request_list_friend = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name", "order", "hints"));
+
+                request_list_friend.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+
+                        super.onComplete(response);
+
+                        VKList list = new VKList();
+
+                        list = (VKList) response.parsedModel;
+
+                        int id = 0;
+
+                        VKApiModel model = list.get(position);
+                        try {
+                            id = model.fields.getInt("id");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        String firstname = "";
+                        String lastname = "";
+
+
+                        try {
+                            firstname = model.fields.getString("first_name");
+                            lastname = model.fields.getString("last_name");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        FragmentSingleFriend newFragment = FragmentSingleFriend.getInstance(id, firstname, lastname);
+                        fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentPlace, newFragment)
+                        .addToBackStack(null)
+                        .commit();
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/kirill/techpark16/Fragments/FragmentFriendsList.java
+++ app/src/main/java/com/example/kirill/techpark16/Fragments/FragmentFriendsList.java
@@ -1,14 +1,18 @@
 package com.example.kirill.techpark16.Fragments;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,7 +23,10 @@ import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKParameters;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiModel;
 import com.vk.sdk.api.model.VKList;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.zip.Inflater;
@@ -27,41 +34,18 @@ import java.util.zip.Inflater;
 /**
  * Created by konstantin on 09.04.16.
  */
-public class FragmentFriendsList extends android.support.v4.app.ListFragment {
+public class FragmentFriendsList extends Fragment {
 
     RecyclerView recyclerView;
     RecyclerViewAdapter recyclerViewAdapter;
     VKList list = new VKList();
-    private onItemSelectedListener mCallback;
+    FragmentTransaction fragmentTransaction;
 
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        mCallback.onFriendSelected(position);
-    }
-
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-    }
 
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends_list, null);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-
-
-        ArrayList<String> arrayList = new ArrayList<String>();
-        for (int i = 0; i < 10; i++){
-            arrayList.add(String.valueOf(i));
-        }
-
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerViewAdapter = new RecyclerViewAdapter(getActivity(), arrayList);
-        recyclerView.setAdapter(recyclerViewAdapter);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
 
         VKRequest request_list_friend = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS,
                 "first_name, last_name", "order", "hints"));
@@ -74,32 +58,21 @@ public class FragmentFriendsList extends android.support.v4.app.ListFragment {
 
                 list = (VKList) response.parsedModel;
 
-
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerViewAdapter = new RecyclerViewAdapter(getActivity().getSupportFragmentManager(),list);
+                recyclerView.setAdapter(recyclerViewAdapter);
             }
         });
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        VKRequest request_list_friend = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name", "order", "hints"));
-
-        request_list_friend.executeWithListener(new VKRequest.VKRequestListener() {
-            @Override
-            public void onComplete(VKResponse response) {
-
-                super.onComplete(response);
-
-                list = (VKList) response.parsedModel;
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
-                setListAdapter(adapter);
-
-            }
-        });
 
     }
 
@@ -110,25 +83,6 @@ public class FragmentFriendsList extends android.support.v4.app.ListFragment {
         getActivity().findViewById(R.id.toolbar).findViewById(R.id.toolbar_button).setVisibility(View.INVISIBLE);
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
 
-        Activity activity;
-
-        if (context instanceof Activity){
-            activity=(Activity) context;
-            try {
-                mCallback = (onItemSelectedListener) activity;
-            } catch (ClassCastException e) {
-                throw new ClassCastException(activity.toString()
-                        + " must implement OnItemSelectedListener");
-            }
-        }
-    }
-
-    public interface onItemSelectedListener {
-        public void onFriendSelected(int position);
-    }
 
 }
